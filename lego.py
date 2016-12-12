cce531376ca30137050641d04e0a14c384a4062b
WhittKinley <whittkinley@gmail.com>
2016-12-12 00:13:25 -0600

    a

--- lego.py
+++ lego.py
@@ -21,7 +21,7 @@ rightTire = LargeMotor(OUTPUT_D)
 # #def findObject():
 # while getUltrasonic > 5.5:
 
-ev3.sound.speak("Hello Ali")
+ev3.Sound.speak("Hello Ali")
 
 leftTire.run_timed(speed_sp = 360, time_sp = 600)
 rightTire.run_timed(speed_sp = 360, time_sp = 600)
